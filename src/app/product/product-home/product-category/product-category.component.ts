0f95be45e0bb8e6a4a022abc7a2b31c21a227892
abesselem18275 <abesselem.idani@gmail.com>
2019-05-25 19:00:33 +0100

    addanimation to collection

--- src/app/product/product-home/product-category/product-category.component.ts
+++ src/app/product/product-home/product-category/product-category.component.ts
@@ -9,20 +9,38 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
   styleUrls: ['./product-category.component.scss'],
   animations: [
     trigger('imageChangeTrigger', [
-      state('in', style({
+      state('inLeft', style({
         opacity : '1'
       })),
-      state('out', style({
+      state('outLeft', style({
         opacity : '0'
       })),
-      transition('in => out', [
-        animate('0.1s', style({
-          opacity : '0'
+      state('inRight', style({
+        opacity : '1'
+      })),
+      state('outRight', style({
+        opacity : '0'
+      })),
+      transition('* => outRight', [
+        animate('0.2s', style({
+          transform : 'translateX(100%)'
+
         }))
       ]),
-      transition('out => in', [
-        animate('0.1s', style({
-          opacity : '1'
+      transition('* => outLeft', [
+        animate('0.2s', style({
+          transform : 'translateX(-150%)'
+
+        }))
+      ]),
+      transition('* => inRight', [
+        animate('0.2s', style({
+          transform : 'translateX(50%)'
+        }))
+      ]),
+      transition('* => inRight', [
+        animate('0.2s', style({
+          transform : 'translateX(50%)'
         }))
       ]),
     ]),
@@ -37,30 +55,65 @@ export class ProductCategoryComponent implements OnInit {
   selectedImage: string;
   isReady: boolean;
   isImageReady: boolean;
+  isRightChange: boolean;
   constructor(private pds: ProductDataService) { }
 
   ngOnInit() {
+    this.isReady = false;
     this.selectedIndex = 0;
     this.hoveredDescription = '-';
-    this.collections = this.pds.getCollections();
-    this.imagesNumber = this.collections.length;
+
+    this.pds.get_elements({model: 'collection'}).subscribe(_collections => {
+      this.collections = _collections;
+      this.imagesNumber = this.collections.length;
+      this.isReady = true;
+      this.updateImage(this.selectedIndex);
+     });
   }
+
+
   toggleCollection(i) {
     this.selectedIndex = i ;
     this.hoveredDescription = i === -1 ? '-' : this.collections[i].description;
   }
+
+  updateImage(index) {
+    this.isImageReady = false;
+    console.warn(this.getState(), this.isRightChange, this.isImageReady);
+    this.selectedIndex = index;
+    setTimeout(() => {
+      this.isImageReady = true;
+      this.hoveredDescription = index === -1 ? '-' : this.collections[index].description;
+      this.selectedImage = index === -1 ? '' : this.collections[index].thumbNail.content;
+      console.warn(this.getState(), this.isRightChange, this.isImageReady);
+    }, 300);
+  }
+
   stepUpdateImage(step) {
-    this.selectedIndex = this.selectedIndex + step;
-    if ( this.selectedIndex >= this.imagesNumber  ) {
-      this.selectedIndex = 0;
+    this.isRightChange = step === -1 ? false : true;
+    let _index = this.selectedIndex + step;
+    if ( _index >= this.imagesNumber  ) {
+      _index = 0;
     }
-    if ( this.selectedIndex < 0  ) {
-      this.selectedIndex = this.imagesNumber - 1;
+    if ( _index < 0  ) {
+      _index = this.imagesNumber - 1;
     }
-    this.toggleCollection(this.selectedIndex);
+    this.updateImage(_index);
   }
   counter() {
 
     return new Array(this.imagesNumber);
+  }
+  getState() {
+    if (this.isImageReady) {
+
+          return 'inRight';
+    }  else {
+      if (this.isRightChange) {
+        return 'outRight';
+      } else {
+        return 'outLeft';
+      }
+    }
   }
 }
