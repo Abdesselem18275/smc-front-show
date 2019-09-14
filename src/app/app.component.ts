bc3b24e1f190d403e8647ab5f58c17e7d7b9f1e1
abesselem18275 <abesselem.idani@gmail.com>
2019-09-14 22:04:20 +0100

    rubbon on menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,10 +11,7 @@ export class AppComponent implements OnInit {
   isSideMenuActive: boolean;
 
   constructor( iconRegistry: MdcIconRegistry, sanitizer: DomSanitizer) {
-    iconRegistry.addSvgIcon('Logo', sanitizer.bypassSecurityTrustResourceUrl('./assets/icons/logo.svg'))
-                .addSvgIcon('Telephone', sanitizer.bypassSecurityTrustResourceUrl('./assets/icons/telephone_icon.svg'))
-                .addSvgIcon('Team', sanitizer.bypassSecurityTrustResourceUrl('./assets/icons/team_icon.svg'))
-                .addSvgIcon('Bar_code', sanitizer.bypassSecurityTrustResourceUrl('./assets/icons/barcode_icon.svg'))
+    iconRegistry.addSvgIcon('Logo_inverted', sanitizer.bypassSecurityTrustResourceUrl('./assets/icons/logo_inverted.svg'))
                 .addSvgIcon('rectangle', sanitizer.bypassSecurityTrustResourceUrl('./assets/icons/rectangle.svg'))
                 .addSvgIcon('loading', sanitizer.bypassSecurityTrustResourceUrl('./assets/icons/loading_logo.svg'));
 
