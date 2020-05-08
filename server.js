dc300e0693e86c8ed44bebebbc34cf4758020886
Abdesselem Idani <abdesselem.idani@gmail.com>
2020-05-08 12:40:33 +0100

    remove internationalisation support

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 //Install express server
 const compression = require('compression')
 const express = require('express');
@@ -8,7 +7,7 @@ const app = express();
 app.use(compression())
 
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').load();
+    require('dotenv').config();
   }
 
 
@@ -16,22 +15,27 @@ if (process.env.NODE_ENV !== 'production') {
 app.use(express.static('./dist/'));
 
 
-app.get('/*', function(req,res) {
-  const supportedLocales = ['en','fr','de'];
-  const defaultLocale = 'en';
-  //const matches = req.url.match(/^\/([a-z]{2}(?:-[A-Z]{2})?)\//);
-  const matches = req.url.match(/(^\/\w{2})(\/\w{2})*/).filter(x => x !== undefined).map(x => x.toLowerCase().replace('/',''));
-  console.log('url ='+req.url);
-  console.log('matches ='+ matches);
-  console.log('base ='+ req.baseUrl);
+// app.get('/*', function(req,res) {
+//   const supportedLocales = ['en','fr','de'];
+//   const defaultLocale = 'en';
+//   //const matches = req.url.match(/^\/([a-z]{2}(?:-[A-Z]{2})?)\//);
+//   const matches = req.url.match(/(^\/\w{2})(\/\w{2})*/).filter(x => x !== undefined).map(x => x.toLowerCase().replace('/',''));
+//   console.log('url ='+req.url);
+//   console.log('matches ='+ matches);
+//   console.log('base ='+ req.baseUrl);
 
-  console.log('last ='+ matches[matches.length-1]);
-  //check if the requested url has a correct format '/locale' and matches any of the supportedLocales
-  const locale = (matches && supportedLocales.indexOf(matches[matches.length-1]) !== -1) ? 
-                  matches[matches.length-1] : defaultLocale;
-  console.warn(locale);
-  console.warn(path.join(__dirname,'dist',locale,'index.html'));
-  res.sendFile(path.join(__dirname,'dist',locale,'index.html'));
+//   console.log('last ='+ matches[matches.length-1]);
+//   //check if the requested url has a correct format '/locale' and matches any of the supportedLocales
+//   const locale = (matches && supportedLocales.indexOf(matches[matches.length-1]) !== -1) ?
+//                   matches[matches.length-1] : defaultLocale;
+//   console.warn(locale);
+//   console.warn(path.join(__dirname,'dist',locale,'index.html'));
+//   res.sendFile(path.join(__dirname,'dist',locale,'index.html'));
+// });
+
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname,'dist/en/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
