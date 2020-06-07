807264d3155570270eb1699aea816ae50307cc6f
Abdesselem Idani <abdesselem.idani@gmail.com>
2020-06-07 15:03:19 +0100

    fix (product dimension component)
    - Now dimension are presented as list where each list item contain the measure type + measure unit and the list of variant measures

--- src/app/product/product-dimensions/product-dimensions.component.ts
+++ src/app/product/product-dimensions/product-dimensions.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { DimensionsSpecification, MeasureType } from '../model';
+import {transpose} from '../../../utils/util-functions';
 @Component({
   selector: 'app-product-dimensions',
   templateUrl: './product-dimensions.component.html',
   styleUrls: ['./product-dimensions.component.scss']
 })
 export class ProductDimensionsComponent implements OnInit {
-
+  @Input() productDimensions : DimensionsSpecification[]
+  headersList : MeasureType[]
   constructor() { }
 
   ngOnInit(): void {
+    this.headersList = this.productDimensions.map(x => x.measureType)
   }
+
+  dimensionsMatrix()  {
+    return transpose(this.productDimensions.map(x => x.measures))}
+  variantsNumber() {
+      return this.productDimensions[0].measures.length
+    }
 
 }
