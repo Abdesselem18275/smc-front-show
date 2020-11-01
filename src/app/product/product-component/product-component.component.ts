1b2c862659548778cc114faa0f648d201b3f4d9e
abesselem18275 <abesselem.idani@gmail.com>
2020-11-01 14:10:47 +0100

    style minor style change

--- src/app/product/product-component/product-component.component.ts
+++ src/app/product/product-component/product-component.component.ts
@@ -5,6 +5,7 @@ import { ComponentsSpecification } from '../../models/product.models';
   selector: 'app-product-component',
   changeDetection : ChangeDetectionStrategy.OnPush,
   templateUrl: './product-component.component.html',
+  styleUrls: ['./product-component.component.scss']
 })
 export class ProductComponentComponent{
   @Input() componentsSpecs : ComponentsSpecification[];
