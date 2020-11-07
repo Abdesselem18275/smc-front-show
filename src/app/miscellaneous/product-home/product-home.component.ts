f96a661a715e976d76621d2c36933ba0571f8729
abesselem18275 <abesselem.idani@gmail.com>
2020-11-07 12:34:43 +0100

    fix set placeholder on hero image background until the image is loaded

--- src/app/miscellaneous/product-home/product-home.component.ts
+++ src/app/miscellaneous/product-home/product-home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild} from '@angular/core';
 import { Category } from '../../models/product.models';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { RootStoreState } from 'src/app/root-store';
 import { GlobalStoreSelectors } from 'src/app/root-store/global-store';
@@ -13,12 +13,23 @@ import { GlobalStoreSelectors } from 'src/app/root-store/global-store';
 })
 export class ProductHomeComponent implements OnInit {
   categories$: Observable<Category[]>;
-
-  constructor(private store$: Store<RootStoreState.State>) { }
+  isLoaded$ = new BehaviorSubject<boolean>(false);
+  bgClass: {};
+  constructor(private store$: Store<RootStoreState.State>) {
+    this.isLoaded$.subscribe((x) => {
+      this.bgClass = {
+        'hero-image':  x,
+        'image--pulsing': !x
+      };
+    })
+   }
   ngOnInit() {
 
 
     this.categories$ = this.store$.select(GlobalStoreSelectors.selectRootCategories);
+  }
+  isLoaded() {
+    this.isLoaded$.next(true)
   }
 
 }
