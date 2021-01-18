42660617d98bd6d6b91212ec90749e8e40903666
abesselem18275 <abesselem.idani@gmail.com>
2021-01-18 10:03:47 +0100

    feat changed product Dimension Variant structure

--- src/app/product/product-dimensions/product-dimensions.component.ts
+++ src/app/product/product-dimensions/product-dimensions.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, Input, Output } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
-import { DimensionsSpecification, DimensionElement } from 'src/app/core/types';
-import { HelperService} from '../../shared/service/helper.service';
+import { DimensionVariant, DimensionVariantSpecification } from 'src/app/core/types';
 
 @Component({
   selector: 'app-product-dimensions',
@@ -12,31 +11,46 @@ export class ProductDimensionsComponent implements OnInit {
   @Input()
   mode: 'quart'|'presentation' = 'presentation';
   @Input()
-  @Output()
-  productDimensions: DimensionsSpecification[] = [];
+  dimensionVariants: DimensionVariant[] = [];
   displayedColumns: string[] = [];
   dimensionColumns: string[] = [];
-  dataSource!: MatTableDataSource<DimensionElement>;
-  constructor(private helperS: HelperService) {
+  dataSource!: MatTableDataSource<DimensionVariant>;
+  constructor() {
    }
 
   ngOnInit(): void {
 
-    this.dataSource = new MatTableDataSource<DimensionElement>(this.helperS.toDimensionArray(this.productDimensions));
-    this.dimensionColumns = this.productDimensions.map(x => x.measureType.designation);
+    this.dataSource = new MatTableDataSource<DimensionVariant>(this.dimensionVariants);
+    this.dimensionColumns = Array.from(this.dimensionVariants.reduce(
+      (acc: Set<string>,currentValue: DimensionVariant) => {
+        currentValue.dimensionVariantsSpecs.forEach(dim => {
+          acc.add(dim.measureType.designation);
+        });
+        return acc;
+      }
+
+      ,new Set<string>()));
     this.displayedColumns = this.dimensionColumns.slice();
+    this.displayedColumns.unshift('ref');
     if(!this.isPresentationMode) {
       this.displayedColumns.unshift('quantity');
-    }else {
-      this.dimensionColumns.unshift('ref');
-      this.displayedColumns = this.dimensionColumns.slice();
     }
   }
   variantsNumber() {
-      return this.productDimensions[0].measures.length;
+      return this.dimensionColumns.length;
     }
   tableWidth() {
     return `${this.displayedColumns.length * 80}px`;
+  }
+  getMeasure(variant: DimensionVariant,measureType: string): string {
+      const varIndex = this.dimensionVariants.find((dimVar: DimensionVariant) => dimVar.designation === variant.designation);
+      if (variant) {
+        const measure = varIndex.dimensionVariantsSpecs.find((dimSpecs: DimensionVariantSpecification) =>
+          dimSpecs.measureType.designation === measureType );
+        return measure ? `${measure.value} ${measure.measureType.unit}` : '-';
+      } else {
+        return '-';
+      }
   }
   get isPresentationMode(): boolean {
     return this.mode === 'presentation';
