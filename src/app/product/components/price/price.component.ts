5f9781d03402a157eb2f7ac0028682aa0e590848
Abdesselem Idani <abdesselem.idani@gmail.com>
2021-02-12 12:58:39 +0100

    fix price dispaly

--- src/app/product/components/price/price.component.ts
+++ src/app/product/components/price/price.component.ts
@@ -1,7 +1,10 @@
 import { BreakpointObserver } from '@angular/cdk/layout';
 import { Input } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Product } from 'src/app/models/product.models';
+import { Currency } from 'src/app/models/shared.models';
+import { GlobalStateService } from 'src/app/shared/state/global-state.service';
 
 @Component({
   selector: 'app-price',
@@ -11,10 +14,12 @@ import { Product } from 'src/app/models/product.models';
 export class PriceComponent implements OnInit {
   @Input() product: Product;
   isSmallScreen: boolean;
+  currency$:Observable<Currency>;
   minPrice = 1378.5;
   maxPrice = 1380.5;
-  constructor(breakpointObserver: BreakpointObserver) {
+  constructor(private gss: GlobalStateService,breakpointObserver: BreakpointObserver) {
     this.isSmallScreen = breakpointObserver.isMatched('(max-width: 720px)');
+    this.currency$ = this.gss.userPaimentCurrency
 
    }
 
