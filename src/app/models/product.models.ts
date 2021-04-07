ac9688279216f7f0103f01660310435cee2207d8
Abdesselem Idani <abdesselem.idani@gmail.com>
2021-04-07 08:03:25 +0100

    feat replaced interface by class for model with buisiness logic

--- src/app/models/product.models.ts
+++ src/app/models/product.models.ts
@@ -22,7 +22,23 @@ export interface Choice {
     value: string;
     checked: boolean;
 }
-export type Product = {
+export interface ApiProduct {
+    id: number;
+    designation: string;
+    short_description: string;
+    interior_coatings: string[];
+    long_description: string;
+    raw_materials: string[];
+    root_category: number;
+    dimension_variants: ApiDimensionVariant[];
+    appearance_variants: AppearanceVariant[];
+    components_specifications: ComponentsSpecification[];
+    features: Feature[];
+    min_price: number;
+    max_price: number;
+};
+
+export class Product {
     id: number;
     designation: string;
     shortDescription: string;
@@ -36,22 +52,68 @@ export type Product = {
     features: Feature[];
     minPrice: number;
     maxPrice: number;
-};
+    constructor(args:ApiProduct) {
+        this.id = args.id
+        this.designation = args.designation
+        this.shortDescription = args.short_description
+        this.interiorCoatings = args.interior_coatings
+        this.longDescription = args.long_description
+        this.rawMaterials = args.raw_materials
+        this.rootCategory = args.root_category
+        this.dimensionVariants = args.dimension_variants ?  args.dimension_variants.map(dim => new DimensionVariant(dim)) : []
+        this.appearanceVariants = args.appearance_variants
+        this.componentsSpecifications = args.components_specifications
+        this.features = args.features
+        this.minPrice = args.min_price
+        this.maxPrice = args.max_price     
+    }
+}
 
-export interface DimensionVariantSpecification {
-    value: number[];
+export interface ApiDimensionVariantSpecification {
+    value: number;
+    measure_type: MeasureType;
+    is_Master_specification: boolean;
+}
+
+export class DimensionVariantSpecification {
+    value: number;
     measureType: MeasureType;
     isMasterSpecification: boolean;
+    constructor(args:ApiDimensionVariantSpecification) {
+        this.value = args.value
+        this.measureType = args.measure_type
+        this.isMasterSpecification = args.is_Master_specification
+    }
 }
-export interface DimensionVariant {
+export interface ApiDimensionVariant {
+    id:number;
+    designation: string;
+    dimensionVariantsSpecs: DimensionVariantSpecification[];
+  }
+export class DimensionVariant {
+  id:number;
   designation: string;
   dimensionVariantsSpecs: DimensionVariantSpecification[];
+  constructor(args:ApiDimensionVariant) {
+      Object.assign(this,args)
+  }
 }
-export interface MeasureType {
+export interface ApiMeasureType {
    designation: string;
-   getShortDesignation: string;
+   short_designation: string;
    unit: string;
 }
+
+export class MeasureType {
+    designation: string;
+    getShortDesignation: string;
+    unit: string;
+    constructor(args:ApiMeasureType){
+        Object.assign(this,args)
+        this.getShortDesignation = args.short_designation
+    }
+ }
+
 export interface AppearanceVariant {
     id?: number;
     thumbNail?: string;
@@ -79,7 +141,19 @@ export interface BaseImage {
     content: string;
     designation: string;
 }
-export interface Category   {
+export interface ApiCategory   {
+    id?: number;
+    designation: string;
+    description?: string;
+    parent_category?: number;
+    is_root?: boolean;
+    is_leaf?: boolean;
+    children?: Category[];
+    thumbNail?: string;
+    thumbNail_aux?: string;
+    svgIcon?: string;
+}
+export class Category   {
     id?: number;
     designation: string;
     description?: string;
@@ -90,6 +164,14 @@ export interface Category   {
     thumbNail?: string;
     thumbNailAux?: string;
     svgIcon?: string;
+    constructor(args:ApiCategory) {
+        Object.assign(this,args)
+        this.thumbNailAux = args.thumbNail_aux
+        this.parentCategory = args.parent_category
+        this.isLeaf = args.is_leaf
+        this.isRoot = args.is_root
+        this.svgIcon = args.svgIcon
+    }
 }
 
 export interface Feature {
@@ -114,5 +196,9 @@ export interface MenuTreeData {
     [key: string]: string;
   }
 
-export type PaginatedProductsType = {count: number;next: string;previous: string;results: Product[]};
+export type PaginatedObjectsType<T> = {
+    count: number;
+    next: string;
+    previous: string;
+    results: T[]};
 
